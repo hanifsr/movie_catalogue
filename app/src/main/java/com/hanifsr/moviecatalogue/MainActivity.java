80dcf3c6af326f49cdfa5aceb96b643f0b3aac46
hanifsr <hanifsr96@gmail.com>
2019-08-25 18:52:57 +0700

    Added movie posters in drawable. Added movie resources in strings.xml. Added ListView and Adapter.

--- app/src/main/java/com/hanifsr/moviecatalogue/MainActivity.java
+++ app/src/main/java/com/hanifsr/moviecatalogue/MainActivity.java
@@ -2,13 +2,58 @@ package com.hanifsr.moviecatalogue;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+	private TypedArray dataPoster;
+	private String[] dataTitle, dataGenres;
+	private MovieAdapter movieAdapter;
+	private ArrayList<Movie> movies;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		movieAdapter = new MovieAdapter(this);
+		ListView listView = findViewById(R.id.movie_list);
+		listView.setAdapter(movieAdapter);
+
+		prepare();
+		addItem();
+
+		listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Toast.makeText(MainActivity.this, movies.get(position).getTitle(), Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+
+	private void addItem() {
+		movies = new ArrayList<>();
+
+		for (int i = 0; i < dataTitle.length; i++) {
+			Movie movie = new Movie();
+			movie.setPoster(dataPoster.getResourceId(i, -1));
+			movie.setTitle(dataTitle[i]);
+			movie.setGenres(dataGenres[i]);
+			movies.add(movie);
+		}
+		movieAdapter.setMovies(movies);
+	}
+
+	private void prepare() {
+		dataTitle = getResources().getStringArray(R.array.data_title);
+		dataGenres = getResources().getStringArray(R.array.data_genres);
+		dataPoster = getResources().obtainTypedArray(R.array.data_poster);
 	}
 }
---
+++ app/src/main/java/com/hanifsr/moviecatalogue/Movie.java
@@ -0,0 +1,107 @@
+package com.hanifsr.moviecatalogue;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Movie implements Parcelable {
+
+	private int poster;
+	private String title, overview, genres, dateRelease, rating, runtime;
+
+	public int getPoster() {
+		return poster;
+	}
+
+	public void setPoster(int poster) {
+		this.poster = poster;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getOverview() {
+		return overview;
+	}
+
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
+
+	public String getGenres() {
+		return genres;
+	}
+
+	public void setGenres(String genres) {
+		this.genres = genres;
+	}
+
+	public String getDateRelease() {
+		return dateRelease;
+	}
+
+	public void setDateRelease(String dateRelease) {
+		this.dateRelease = dateRelease;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public String getRuntime() {
+		return runtime;
+	}
+
+	public void setRuntime(String runtime) {
+		this.runtime = runtime;
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeInt(this.poster);
+		dest.writeString(this.title);
+		dest.writeString(this.overview);
+		dest.writeString(this.genres);
+		dest.writeString(this.dateRelease);
+		dest.writeString(this.rating);
+		dest.writeString(this.runtime);
+	}
+
+	public Movie() {
+	}
+
+	protected Movie(Parcel in) {
+		this.poster = in.readInt();
+		this.title = in.readString();
+		this.overview = in.readString();
+		this.genres = in.readString();
+		this.dateRelease = in.readString();
+		this.rating = in.readString();
+		this.runtime = in.readString();
+	}
+
+	public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+		@Override
+		public Movie createFromParcel(Parcel source) {
+			return new Movie(source);
+		}
+
+		@Override
+		public Movie[] newArray(int size) {
+			return new Movie[size];
+		}
+	};
+}
